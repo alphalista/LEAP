1bfc496401556a601f30922a6c36114c08bbea03
Muon05 <dbfugks@naver.com>
2024-11-06 00:34:28 +0900

    refactor: login logic refactoring

--- kpaas_task/auth/urls.py
+++ kpaas_task/auth/urls.py
@@ -3,5 +3,4 @@ from .views import kakao_callback
 
 urlpatterns = [
     path('login/kakao-callback', kakao_callback, name='kakao_callback'),
-    # path('seongmin', seongmin, name='seongmin'),
 ]
--- kpaas_task/auth/views.py
+++ kpaas_task/auth/views.py
@@ -9,23 +9,14 @@ def kakao_callback(request):
     code = request.GET.get('code')
     redirect_uri = 'http://127.0.0.1:8000/auth/login/kakao-callback'
     url = 'https://kauth.kakao.com/oauth/token'
-    test_uri = 'http://127.0.0.1:8000/auth/seongmin'
-    csrf_token = get_token(request)
     
-    key = KAKAO_REST_API_KEY
-
     headers = {
         'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
     }
 
-    test_headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'X-CSRFToken': csrf_token
-    }
-
     data = {
         'grant_type' : 'authorization_code',
-        'client_id' : key,
+        'client_id' : KAKAO_REST_API_KEY,
         'redirect_uri' : redirect_uri,
         'code' : code,
         'client_secret' : KAKAO_CLIENT_SECRET
@@ -35,12 +26,8 @@ def kakao_callback(request):
     
     if response.status_code == 200:
         try:
-            # post_response = requests.post(test_uri, headers=test_headers, data={"message": "hi"})
             return JsonResponse(response.json())
         except ValueError:
             return JsonResponse({"error": "Invalid JSON response", "content": response.text}, status=500)
     else:
         return JsonResponse({"response": response.text, "hhhh":data}, status=response.status_code,)
-
-# def seongmin(request):
-#     print("hi")
