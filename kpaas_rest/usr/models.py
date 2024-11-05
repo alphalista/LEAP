8392e846556093437f5078b65dbb613d1fbd0369
YimTaeKeun <ytk030305@gmail.com>
2024-11-05 22:29:07 +0900

    [FIX]: Rename PK

--- kpaas_rest/usr/models.py
+++ kpaas_rest/usr/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Users(models.Model):
-    user_pk = models.CharField(max_length=100, primary_key=True)
+    user_id = models.CharField(max_length=100, primary_key=True)
     nickname = models.CharField(max_length=100)
 
     class Meta:
--- kpaas_rest/usr/views.py
+++ kpaas_rest/usr/views.py
@@ -10,5 +10,5 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.query_params.get('user', None)
         if user:
-            return self.queryset.filter(user_pk=user)
+            return self.queryset.filter(user_id=user)
         return self.queryset
