0b68a89f3783c0ae7382fd08a90c9c2349b44da3
YimTaeKeun <ytk030305@gmail.com>
2024-11-24 19:45:42 +0900

    MODIFY: Remove debuging code

--- kpaas_task/crawling/OTC_bond_scrapy/spiders/kiwoomSpider.py
+++ kpaas_task/crawling/OTC_bond_scrapy/spiders/kiwoomSpider.py
@@ -52,6 +52,6 @@ class KiwoomspiderSpider(scrapy.Spider):
         item['interest_percentage'] = response.xpath('//*[contains(@class, \'modal-body-container\')]/div/section/div[2]/div/table/tbody/tr[4]/td[1]/text()').extract_first().replace('%', '')
         item['danger_degree'] = response.xpath('//*[contains(@class, \'modal-body-container\')]/div/section/div[2]/div/table/tbody/tr[8]/td[2]/text()').extract_first().replace(' ', '')
         # item['nxt_int_date'] = response.xpath('//*[contains(@class, \'modal-body-container\')]/div/section/div[2]/div/table/tbody/tr[8]/td[2]/text()').extract_first()
-        print(item)
+        # print(item)
         yield item
 
